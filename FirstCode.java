988ffaa9e247f25507ff1dfe00516dcab1558658
“pppatil160” <pppatil160@gmail.com>
2025-04-07 15:35:32 -1200

    My second add code

---
+++ FirstCode.java
@@ -0,0 +1,18 @@
+package SecondProject;
+
+public class FirstCode {
+ 
+	public static void main(String[] args) {
+		
+		int Mark=30;
+		 
+		 if(Mark<=30) {
+			 System.out.println("Pass Pratik"); 
+		 }
+		 
+		 else {
+			 System.out.println("Fail Patil");
+		 } 	
+		
+	}
+}
